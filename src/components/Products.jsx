1e07458bee82444f7a94d7762c921bc09b4f6a20
Jose Luis <pepirob>
2023-06-16 19:19:11 +0200

    practice done

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,13 +1,26 @@
 /* eslint-disable react/prop-types */
+import { useFilters } from "../hooks/useFilters";
 import "./Products.css";
-import { AddToCartIcon } from "./icons";
+import { AddToCartIcon, RemoveFromCartIcon } from "./icons";
+import { products as initialProducts } from "../mocks/products.json";
+import { useCart } from "../hooks/useCart";
 
-export function Products({ products }) {
+export function Products() {
+  const { filterProducts } = useFilters();
+  const { cart, addToCart, removeFromCart, clearCart } = useCart();
+
+  const checkProductInCart = (product) => {
+    return cart.some((item) => item.id === product.id);
+  };
+
+  const products = filterProducts(initialProducts);
+
   return (
     <>
       <main className="products">
         <ul>
           {products.slice(0, 10).map((product) => {
+            const isProductInCart = checkProductInCart(product);
             return (
               <li key={product.id}>
                 <img src={product.thumbnail} alt={product.title} />
@@ -15,8 +28,21 @@ export function Products({ products }) {
                   <strong>{product.title} </strong> - ${product.price}
                 </div>
                 <div>
-                  <button>
-                    <AddToCartIcon />
+                  <button
+                    style={{
+                      backgroundColor: isProductInCart ? "red" : "#09f",
+                    }}
+                    onClick={() =>
+                      isProductInCart
+                        ? removeFromCart(product)
+                        : addToCart(product)
+                    }
+                  >
+                    {isProductInCart ? (
+                      <RemoveFromCartIcon />
+                    ) : (
+                      <AddToCartIcon />
+                    )}
                   </button>
                 </div>
               </li>
